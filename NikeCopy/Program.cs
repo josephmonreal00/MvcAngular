49a8b4cbeaa70265b13eb0e182b38177c3ecae04
Joseph_Monreal <Joseph_Monreal@uhaul.com>
2022-10-26 14:07:15 -0700

    Added Startup.cs option

--- NikeCopy/Program.cs
+++ NikeCopy/Program.cs
@@ -1,6 +1,15 @@
+using NikeCopy;
+
+//var builder = WebApplication.CreateBuilder(args);
+//var app = builder.Build();
+
+//app.MapGet("/", () => "Hello World!");
+
+//app.Run();
+
+// If not using .Net 6 and want to use Startup.cs
 var builder = WebApplication.CreateBuilder(args);
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services); // calling ConfigureServices method
 var app = builder.Build();
-
-app.MapGet("/", () => "Hello World!");
-
-app.Run();
+startup.Configure(app, builder.Environment); // calling Configure method
---
+++ NikeCopy/Startup.cs
@@ -0,0 +1,51 @@
+﻿namespace NikeCopy
+{
+    public class Startup
+    {
+        public IConfiguration ConfigRoot { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            ConfigRoot = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment()) {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+            //app.UseHttpsRedirection();
+
+            // Looks through files to find index.html
+            // changes internal path to where that file exists
+            app.UseDefaultFiles(); 
+
+            // That path is then passed to UseStaticFiles()
+            // and is able to find static files
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World");
+                });
+            });
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.Run();
+
+
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello World");
+            //});
+        }
+    }
+}
